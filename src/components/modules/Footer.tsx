245c83e1214b431cc0ea0a1acd8262b27258ae96
Nikita Maslov <maslov.n.e@gmail.com>
2025-09-03 22:05:33 +0300

    fix footer

--- src/components/modules/Footer.tsx
+++ src/components/modules/Footer.tsx
@@ -7,18 +7,18 @@ function Footer(): ReactElement {
       className='transition-opacity  duration-300  mt-16  mb-7  lg:mt-24  lg:mb-20'
       id='footer'
     >
-      <MainContainer className='sm:flex  sm:justify-between'>
+      <MainContainer className='md:flex  items-start  sm:justify-between'>
         <ul
           className='flex  flex-col  items-center  gap-2  text-[#91A0B6]  
-                     sm:flex-row  sm:gap-4  sm:items-start'
+                     md:flex-row  md:gap-4'
         >
           <li className='cursor-pointer'>Terms of Service</li>
-          <li className='hidden  w-px  h-4  bg-[#A9B6C8]  self-center  sm:block'></li>
+          <li className='hidden  w-px  h-4  bg-[#A9B6C8]  md:block'></li>
           <li className='cursor-pointer'>Privacy Policy</li>
-          <li className='hidden  w-px  h-4  bg-[#A9B6C8]  self-center  sm:block'></li>
+          <li className='hidden  w-px  h-4  bg-[#A9B6C8]  md:block'></li>
           <li className='cursor-pointer'>Subscription Policy</li>
         </ul>
-        <p className='text-center  text-[#91A0B6]  mt-6  sm:text-left  sm:mt-0'>
+        <p className='text-center  text-[#91A0B6]  mt-6  md:text-left  sm:mt-0'>
           ©Copyright © 2025 PlantIn. <br className='lg:hidden' />
           All rights reserved
         </p>
